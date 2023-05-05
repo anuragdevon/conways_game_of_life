c3c056b7f4c7fb4e1b5d674e0cad150b68c17164
Snail Mail <>
2023-05-05 14:13:58 +0530

    add red test for valid grid creation

--- lifematrix/lifematrix_test.go
+++ lifematrix/lifematrix_test.go
@@ -1,1 +1,17 @@
 package lifematrix
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidNewGridCreationWithPositiveDimension(t *testing.T) {
+	rows := 10
+	cols := 10
+	test_grid, err := NewGrid(rows, cols)
+	assert.Nil(t, err)
+	if test_grid.rows != rows || test_grid.cols != cols {
+		t.Errorf("expected a grid of %d rows and %d cols", rows, cols)
+	}
+}
