b27fc4dd365812845169a4b8a03d754164148313
Snail Mail <>
2023-05-05 14:15:40 +0530

    green: create constructor for grid creation

--- lifematrix/lifematrix.go
+++ lifematrix/lifematrix.go
@@ -1,1 +1,24 @@
 package lifematrix
+
+import "errors"
+
+type Grid struct {
+	rows  int
+	cols  int
+	cells [][]int
+}
+
+func NewGrid(rows, cols int) (*Grid, error) {
+	if rows <= 0 && cols <= 0 {
+		return &Grid{}, errors.New("invalid rows or cols values")
+	}
+	newLifeMatix := &Grid{
+		rows:  rows,
+		cols:  cols,
+		cells: make([][]int, rows),
+	}
+	for row := range newLifeMatix.cells {
+		newLifeMatix.cells[row] = make([]int, cols)
+	}
+	return newLifeMatix, nil
+}
