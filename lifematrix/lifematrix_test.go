be96f76ee42b99239a35b4be6b08ccbd14707c2a
Snail Mail <>
2023-05-05 14:25:19 +0530

    red: add valid randomize cell testcase

--- lifematrix/lifematrix_test.go
+++ lifematrix/lifematrix_test.go
@@ -15,3 +15,33 @@ func TestValidNewGridCreationWithPositiveDimension(t *testing.T) {
 		t.Errorf("expected a grid of %d rows and %d cols", rows, cols)
 	}
 }
+
+func TestInvalidGridCreationWithZeroDimension(t *testing.T) {
+	rows := 0
+	cols := 0
+	_, err := NewGrid(rows, cols)
+	assert.NotNil(t, err)
+}
+
+func TestInvalidGridCreationWithNegativeDimension(t *testing.T) {
+	rows := -1
+	cols := 0
+	_, err := NewGrid(rows, cols)
+	assert.NotNil(t, err)
+}
+
+func TestValidRandomCellValuesInitialization(t *testing.T) {
+	rows := 10
+	cols := 10
+	test_grid, err := NewGrid(rows, cols)
+	assert.Nil(t, err)
+
+	test_grid.Randomize()
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if test_grid.cells[i][j] != 0 && test_grid.cells[i][j] != 1 {
+				t.Error("expected 0 or 1, but got %d", test_grid.cells[i][j])
+			}
+		}
+	}
+}
