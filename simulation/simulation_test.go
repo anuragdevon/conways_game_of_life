5accac0c53fdf4c84a352c3b1dce9ea4d78ccc7a
Snail Mail <>
2023-05-08 08:22:11 +0530

    red: add testcase for overpopulation

--- simulation/simulation_test.go
+++ simulation/simulation_test.go
@@ -98,3 +98,33 @@ func TestLiveCellToLiveForExactlyTwoOrThreeNeighbors(t *testing.T) {
 	testSimulation.nextStateOfSimulation()
 	assert.Equal(t, expectedNextStateGrid, testSimulation.grid.Cells)
 }
+
+func TestLiveCellToDieOfOverpopulation(t *testing.T) {
+	rows := 3
+	cols := 3
+	testGrid, err := lifematrix.NewGrid(rows, cols)
+	assert.Nil(t, err)
+
+	testGrid.SetCellAlive(0, 0, 0)
+	testGrid.SetCellAlive(0, 1, 1)
+	testGrid.SetCellAlive(0, 2, 0)
+
+	testGrid.SetCellAlive(1, 0, 0)
+	testGrid.SetCellAlive(1, 1, 1)
+	testGrid.SetCellAlive(1, 2, 1)
+
+	testGrid.SetCellAlive(2, 0, 1)
+	testGrid.SetCellAlive(2, 1, 1)
+	testGrid.SetCellAlive(2, 2, 1)
+
+	testSimulation := NewSimulation(testGrid)
+
+	expectedNextStateGrid := [][]int{
+		{0, 1, 0},
+		{0, 0, 1},
+		{0, 1, 1},
+	}
+
+	testSimulation.nextStateOfSimulation()
+	assert.Equal(t, expectedNextStateGrid, testSimulation.grid.Cells)
+}
